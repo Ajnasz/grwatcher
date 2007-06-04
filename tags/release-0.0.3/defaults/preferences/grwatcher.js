75531760ef6222e2a38b613cf4ad2725f9d8d00d
None <devnull@localhost>
2007-06-04 20:53:22 +0200

    [svn] Google Reader Watcher v0.0.3 tag initial import

---
+++ tags/release-0.0.3/defaults/preferences/grwatcher.js
@@ -0,0 +1,9 @@
+pref("extensions.grwatcher.checkfreq", 5);
+pref("extensions.grwatcher.openinnewtab", true);
+pref("extensions.grwatcher.resetcounter", false);
+
+pref("extensions.grwatcher.tooltipcounterpos", 'left');
+pref("extensions.grwatcher.tooltiptitlelength", 23);
+
+pref("extensions.grwatcher.email", '');
+pref("extensions.grwatcher.sid", '');
