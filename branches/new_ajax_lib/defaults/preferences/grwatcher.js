fe5fc99dd7c002e0e58bfb3fbc7545404ba8ada7
ajnasz <devnull@localhost>
2007-07-19 11:15:25 +0200

    [svn] create branch to create new ajax lib

---
+++ branches/new_ajax_lib/defaults/preferences/grwatcher.js
@@ -0,0 +1,15 @@
+pref("extensions.grwatcher.checkfreq", 5);
+pref("extensions.grwatcher.openinnewtab", true);
+pref("extensions.grwatcher.resetcounter", false);
+
+pref("extensions.grwatcher.tooltipcounterpos", 'left');
+pref("extensions.grwatcher.tooltiptitlelength", 23);
+
+pref("extensions.grwatcher.sid", '');
+pref("extensions.grwatcher.rememberLogin", false);
+
+pref("extensions.grwatcher.leftclickopen", 1);
+pref("extensions.grwatcher.activateopenedtab", true);
+
+pref("extensions.grwatcher.shownotificationwindow", true);
+pref("extensions.grwatcher.showzerocounter", false);
