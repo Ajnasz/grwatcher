42b307a11057a6700baf6f42745ef3a1ad48f32c
ajnasz <devnull@localhost>
2008-09-08 18:49:51 +0200

    [svn]  - update version to 0.0.13

--- trunk/chrome/content/ajax.js
+++ trunk/chrome/content/ajax.js
@@ -27,7 +27,7 @@ var Ajax = function(pars, parameters) {
   /**
    * @private
    */
-  var agent = 'Google Reader Watcher 0.0.13b';
+  var agent = 'Google Reader Watcher 0.0.13';
 
   this.url = pars.url;
   this.pars = typeof pars.pars != 'undefined' ? pars.pars : this.pars;
