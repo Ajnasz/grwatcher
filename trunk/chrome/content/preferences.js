4ef28006be1f24f144bbf2e8ba6005f5601391a5
ajnasz <devnull@localhost>
2008-10-28 21:48:33 +0100

    [svn]  - add documentation  - add setter functions to forceLogin, sid, delayStart  - add gettern function to sid

--- trunk/chrome/content/preferences.js
+++ trunk/chrome/content/preferences.js
@@ -115,54 +115,123 @@ var GRPrefs = {
     */
     forceLogin: function() {
       return prefManager.getBoolPref('extensions.grwatcher.forcelogin');
+    },
+   /**
+    * @type String
+    */
+    sid: function() {
+      return prefManager.getCharPref('extensions.grwatcher.sid');
     }
   },
   setPref: {
+    /**
+     * @param {Integer} value
+     */
     checkFreq : function(value) {
       prefManager.setIntPref('extensions.grwatcher.checkfreq', value);
     },
+    /**
+     * @param {Boolean} value
+     */
     openInNewTab : function(value) {
       prefManager.setBoolPref('extensions.grwatcher.openinnewtab', value);
     },
+    /**
+     * @param {Boolean} value
+     */
     resetCounter : function(value) {
       prefManager.setBoolPref('extensions.grwatcher.resetcounter', value);
     },
+    /**
+     * @param {String} value
+     */
     tooltipCounterPos : function(value) {
       prefManager.setCharPref('extensions.grwatcher.tooltipcounterpos', value);
     },
+    /**
+     * @param {Integer} value
+     */
     tooltipTitleLength : function(value) {
       prefManager.setIntPref('extensions.grwatcher.tooltiptitlelength', value);
     },
+    /**
+     * @param {Boolean} value
+     */
     rememberLogin : function(value) {
       prefManager.setBoolPref('extensions.grwatcher.rememberLogin', value);
     },
+    /**
+     * @param {Integer} value
+     */
     leftClickOpen : function(value) {
       prefManager.setIntPref('extensions.grwatcher.leftclickopen', value);
     },
+    /**
+     * @param {Boolean} value
+     */
     activateOpenedTab : function(value) {
       prefManager.setBoolPref('extensions.grwatcher.activateopenedtab', value);
     },
+    /**
+     * @param {Boolean} value
+     */
     showNotificationWindow : function(value) {
-    prefManager.setBoolPref('extensions.grwatcher.shownotificationwindow', value);
+      prefManager.setBoolPref('extensions.grwatcher.shownotificationwindow', value);
     },
+    /**
+     * @param {Boolean} value
+     */
     showZeroCounter : function(value) {
-    prefManager.setBoolPref('extensions.grwatcher.showzerocounter', value);
+      prefManager.setBoolPref('extensions.grwatcher.showzerocounter', value);
     },
+    /**
+     * @param {Boolean} value
+     */
     useSecureConnection : function(value) {
-    prefManager.setBoolPref('extensions.grwatcher.usesecureconnection', value);
+      prefManager.setBoolPref('extensions.grwatcher.usesecureconnection', value);
     },
+    /**
+     * @param {String} value
+     */
     userName: function(value) {
       prefManager.setCharPref('extensions.grwatcher.username', value);
     },
+    /**
+     * @param {Boolean} value
+     */
     sortByLabels : function(value) {
       prefManager.setBoolPref('extensions.grwatcher.sortbylabels', value);
     },
+    /**
+     * @param {String} value
+     */
     filteredLabels : function(value) {
       prefManager.setCharPref('extensions.grwatcher.filteredlabels', value);
     },
+    /**
+     * @param {Boolean} value
+     */
     maximizeCounter : function(value) {
       prefManager.setBoolPref('extensions.grwatcher.maximizecounter', value);
     },
+    /**
+     * @param {String} value
+     */
+    sid : function(value) {
+      prefManager.setCharPref('extensions.grwatcher.sid', value);
+    },
+   /**
+    * @param {Boolean} value
+    */
+    forceLogin: function(value) {
+      return prefManager.setBoolPref('extensions.grwatcher.forcelogin', value);
+    },
+   /**
+    * @param {Boolean} value
+    */
+    delayStart: function(value) {
+      return prefManager.setIntPref('extensions.grwatcher.delaystart', value);
+    }
   }
 };
 GRStates = {
