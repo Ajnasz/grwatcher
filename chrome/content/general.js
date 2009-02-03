158561ec758ca0e0bb708284febb8c5e40eccb55
Lajos Koszti <ajnasz@ajnasz.hu>
2009-02-03 22:12:37 +0100

    - add documentation

--- chrome/content/general.js
+++ chrome/content/general.js
@@ -1,5 +1,8 @@
 /**
+ * escaping the special regexp characters, so we won't get an error when try to create a regexp like this: var a = ':)';new RegExp(a);
  * @param {String} str String which special characters should be escaped
+ * @returns the escaped string
+ * @type String
  */
 var GRW_escapeForRegExp = function(str) {
   var specChars = new RegExp('([\\(\\)\\[\\]\\$\\^\\+\\*]+)', 'g');
