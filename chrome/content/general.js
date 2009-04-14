64c5666e9c12df01eda6d0ffbf4c56864d8aac8b
Lajos Koszti <ajnasz@ajnasz.hu>
2009-04-14 07:36:18 +0200

    - add GRW namespace

--- chrome/content/general.js
+++ chrome/content/general.js
@@ -1,10 +1,11 @@
+GRW = {};
 /**
  * escaping the special regexp characters, so we won't get an error when try to create a regexp like this: var a = ':)';new RegExp(a);
  * @param {String} str String which special characters should be escaped
  * @returns the escaped string
  * @type String
  */
-var GRW_escapeForRegExp = function(str) {
+GRW.escapeForRegExp = function(str) {
   var specChars = new RegExp('([\\(\\)\\[\\]\\$\\^\\+\\*]+)', 'g');
   var strA = Array.prototype.slice.call(str);
   return strA.map(function(char){
