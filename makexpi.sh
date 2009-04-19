0c8709ba269d10b49072ce2ad467c17e934b8836
Lajos Koszti <ajnasz@ajnasz.hu>
2009-04-19 10:04:03 +0200

    - version

--- makexpi.sh
+++ makexpi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ########################## Configuration ################################
 if [ -z $1 ];then
-  VER='0.0.5.4a'`date '+%Y%m%d%H%M%S'`;
+  VER='0.0.15a'`date '+%Y%m%d%H%M%S'`;
 else
   VER=$1;
 fi
