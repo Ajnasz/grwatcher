6b58a2430934306a810b4cc6e907d02dd01e0164
Lajos Koszti <ajnasz@ajnasz.hu>
2009-11-17 20:20:11 +0100

    have cookie getter and setter method in one place

--- chrome/content/cookie.js
+++ chrome/content/cookie.js
@@ -1,21 +1,50 @@
-/**
- * @param {String} domain The cookie domain
- * @param {String} name The name of the cookie
- * @param {String} value The value of the cookie
- * @param {Date} [date] (optional) The expires date of the cookie
- * @param {String} [path] (optional) The path of the cookie
- */
-GRW.Cookie = function(domain, name, value, date, path) {
-  var cookieUri = Components.classes["@mozilla.org/network/io-service;1"].getService(Components.interfaces.nsIIOService).newURI('http://' + domain, null, null),
-      cookieSvc = Components.classes["@mozilla.org/cookieService;1"].getService(Components.interfaces.nsICookieService),
-      cookie = name + '=' + value + ';domain=' + domain;
+(function() {
+  /**
+   * @param {String} domain The cookie domain
+   * @param {String} name The name of the cookie
+   * @param {String} value The value of the cookie
+   * @param {Date} [date] (optional) The expires date of the cookie
+   * @param {String} [path] (optional) The path of the cookie
+   */
+  GRW.Cookie = {
+    set: function(domain, name, value, date, path) {
+      var cookieUri = Components.classes["@mozilla.org/network/io-service;1"]
+                        .getService(Components.interfaces.nsIIOService)
+                        .newURI('http://' + domain, null, null),
 
-  if(date instanceof Date) {
-    var days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-    months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-    b = days[date.getUTCDay()] + ' ' + date.getUTCDate() + ' ' + months[date.getUTCMonth()] + ' ' + date.getUTCFullYear() + ' ' + date.getUTCHours() + ':' + date.getUTCMinutes() + ':' + date.getUTCSeconds() + ' UTC';
-    cookie += ';expires=' + b;
-  }
-  cookie += path ? ';path=' + path : ';path=/';
-  cookieSvc.setCookieString(cookieUri, null, cookie, null);
-};
+          cookieSvc = Components.classes["@mozilla.org/cookieService;1"]
+                        .getService(Components.interfaces.nsICookieService),
+
+          cookie = name + '=' + value + ';domain=' + domain;
+
+      if(date instanceof Date) {
+        var days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+        months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+        b = days[date.getUTCDay()] + ' ' + date.getUTCDate() + ' ' + months[date.getUTCMonth()] + ' ' + date.getUTCFullYear() + ' ' + date.getUTCHours() + ':' + date.getUTCMinutes() + ':' + date.getUTCSeconds() + ' UTC';
+        cookie += ';expires=' + b;
+      }
+      cookie += path ? ';path=' + path : ';path=/';
+      cookieSvc.setCookieString(cookieUri, null, cookie, null);
+    },
+
+    get: function(name, domain) {
+      var cookieManager = Components.classes["@mozilla.org/cookiemanager;1"].getService(Components.interfaces.nsICookieManager2),
+          enumerator = cookieManager.enumerator;
+
+      domain = domain || 'google.com';
+
+      var rex = new RegExp(domain + '$'), cookie;
+      while (enumerator.hasMoreElements()) {
+        cookie = enumerator.getNext();
+        if (cookie instanceof Components.interfaces.nsICookie) {
+          if (rex.test(cookie.host)) {
+            if(cookie.name == name) {
+              return cookie.value;
+            }
+          }
+        }
+      }
+      return false;
+    }
+  };
+})();
