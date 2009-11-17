f32b8a2f73696c2473a4fadaddd56ec30081fe57
Lajos Koszti <ajnasz@ajnasz.hu>
2009-11-17 19:22:31 +0100

    remove ff2 compatibility code simplify a bit

--- chrome/content/passmanager.js
+++ chrome/content/passmanager.js
@@ -1,95 +1,52 @@
-/**
- * @author Koszti Lajos [Ajnasz] http://ajnasz.hu ajnasz@ajnasz.hu
- * @license GPL v2
- * for more details see the license.txt
- */
+(function() {
+  var url = 'chrome://grwatcher',
+      username = 'GoogleReaderWatcher',
+    formSubmitURL = 'https://www.google.com',  // not http://www.example.com/foo/auth.cgi
+      loginManager = Components.classes["@mozilla.org/login-manager;1"]
+                      .getService(Components.interfaces.nsILoginManager),
+    nsLoginInfo = new Components.Constructor("@mozilla.org/login-manager/loginInfo;1", Components.interfaces.nsILoginInfo, "init");
 
-/**
- * @constructor
- * @class _PasswordManager Interface to handle easily the user accounts on ff2 and ff3
- * @namespace GRW
- */
-GRW._PasswordManager = function() {
-  this.url = 'chrome://grwatcher';
-  this.username = 'GoogleReaderWatcher';
-
-  try { // Firefox 3
-    this.loginManager = Components.classes["@mozilla.org/login-manager;1"].getService(Components.interfaces.nsILoginManager) || Components.classes["@mozilla.org/passwordmanager;1"].createInstance(Components.interfaces.nsIPasswordManager),
-    this.nsLoginInfo = new Components.Constructor("@mozilla.org/login-manager/loginInfo;1", Components.interfaces.nsILoginInfo, "init");
-
-    this.formSubmitURL = 'https://www.google.com';  // not http://www.example.com/foo/auth.cgi
-
+  /**
+   * Interface to handle easily the user accounts on ff2 and ff3
+   * @constructor
+   * @class _PasswordManager
+   * @namespace GRW
+   */
+  var _PasswordManager = function() {
     this.getPassword = function() {
       try {
         // Find users for the given parameters
-        var logins = this.loginManager.findLogins({}, this.url, this.formSubmitURL, null);
+        var logins = loginManager.findLogins({}, url, formSubmitURL, null);
         // Find user from returned array of nsILoginInfo objects
         for (var i = 0, ll = logins.length; i < ll; i++) {
-          if (logins[i].username == this.username) {
+          if (logins[i].username == username) {
             return logins[i].password;
           }
         }
         return false;
       }
       catch(ex){
-       GRW.log('get pass failed:', ex);
+      GRW.log('get pass failed:', ex);
       }
     };
 
     this.addPassword = function(password) {
       try {
-        var logins = this.loginManager.findLogins({}, this.url, this.formSubmitURL, null);
+        var logins = loginManager.findLogins({}, url, formSubmitURL, null);
         // Find user from returned array of nsILoginInfo objects
         for (var i = 0; i < logins.length; i++) {
-          if (logins[i].username == this.username) {
-            this.loginManager.removeLogin(logins[i]);
+          if (logins[i].username == username) {
+            loginManager.removeLogin(logins[i]);
           }
         }
-        var extLoginInfo = new this.nsLoginInfo(this.url, this.formSubmitURL, null, this.username, password, "", "");
-        this.loginManager.addLogin(extLoginInfo);
+        var extLoginInfo = new nsLoginInfo(url, formSubmitURL, null, username, password, "", "");
+        loginManager.addLogin(extLoginInfo);
       } catch(ex) {
         GRW.log(ex);
       }
 
     }
-  }
-  catch(e) { // Firefox 2
-    // mozilla nsi password manager component
-    this.passwordManager = Components.classes["@mozilla.org/passwordmanager;1"].createInstance(Components.interfaces.nsIPasswordManager);
-
-    this.getPassword = function() {
-      // the host name of the password we are looking for
-      // ask the password manager for an enumerator:
-      var e = this.passwordManager.enumerator;
-      // step through each password in the password manager until we find the one we want:
-      while (e.hasMoreElements()) {
-        try {
-          // get an nsIPassword object out of the password manager.
-          // This contains the actual password...
-          var pass = e.getNext().QueryInterface(Components.interfaces.nsIPassword);
-          if (pass.host == this.url && pass.user == this.username) {
-            return pass.password
-          }
-        } catch (ex){
-          GRW.log(ex);
-        }
-      }
-    },
-    /**
-     * @param {String} password password for the google reader account
-     */
-    this.addPassword = function(password) {
-      try {
-        this.passwordManager.removeUser(this.url, this.username);
-      }
-      catch (ex) {
-        GRW.log(ex);
-      }
-      try {
-        this.passwordManager.addUser(this.url, this.username, password);
-      } catch(ex) {
-        GRW.log(ex);
-      }
-    }
-  }
-};
+  };
+  GRW.PasswordManager = new _PasswordManager();
+  GRW.log('passwordmanagaer');
+})();
