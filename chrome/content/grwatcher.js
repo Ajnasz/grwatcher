b53ba53ff09a4c4aa140fdb9b6ac927ca1b8a535
Lajos Koszti <ajnasz@ajnasz.hu>
2009-11-18 08:28:32 +0100

    get the unread count on init

--- chrome/content/grwatcher.js
+++ chrome/content/grwatcher.js
@@ -3,10 +3,7 @@
  */
 GRW.init = function() {
   GRW.log('Google Reader Watcher ###VERSION### initializitaion started');
-  GRW.log('account exists', GRW.AccountManager.getCurrentSID());
-  GRW.log('account login', GRW.AccountManager.logIn(function() {
-    GRW.log('account exists2', GRW.AccountManager.getCurrentSID());
-  }));
+  new GRW.GetList();
   GRW.log('Google Reader Watcher ###VERSION### initializitaion finished');
 };
 window.addEventListener('load', GRW.init, false);
