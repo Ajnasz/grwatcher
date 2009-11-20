8a7f961028f800af65d910cddc6813f9171f1344
Lajos Koszti <ajnasz@ajnasz.hu>
2009-11-20 23:45:11 +0100

    init things are moved to the request.js, in the grwatcher.js we just start the requests

--- chrome/content/grwatcher.js
+++ chrome/content/grwatcher.js
@@ -2,23 +2,12 @@
  * initialization function
  */
 GRW.init = function() {
-  GRW.log('Google Reader Watcher ###VERSION### initializitaion started');
   GRW.strings = document.getElementById('grwatcher-strings');
-  var getlist = new GRW.GetList();
-  getlist.on('unreadGeneratedEvent', function(elems) {
-    GRW.log('unread generated event');
-  });
-  getlist.on('subscriptionGeneratedEvent', function(elems) {
-    GRW.log('subscription list generated event');
-  });
-  getlist.on('itemsMatchedEvent', function() {
-    GRW.log('items matched event');
-    GRW.Notifier(this._unreadCount.unreadSum);
-  })
-  getlist.on('unreadAndSubscriptionReceivedEvent', function() {
-    this.matchUnreadItems();
-  });
-  getlist.start();
+  GRW.log('Google Reader Watcher ###VERSION### initializitaion started');
+
+  GRW.setTimeout(function() {
+    new GRW.Requester();
+  }, GRW.Prefs.get.delayStart());
   GRW.log('Google Reader Watcher ###VERSION### initializitaion finished');
 };
 window.addEventListener('load', GRW.init, false);
