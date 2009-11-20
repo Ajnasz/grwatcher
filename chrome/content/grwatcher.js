26a19a059343e37c67f95afa01c70b4e67e304ab
Lajos Koszti <ajnasz@ajnasz.hu>
2009-11-20 21:58:48 +0100

    show notification when the unread count received

--- chrome/content/grwatcher.js
+++ chrome/content/grwatcher.js
@@ -3,6 +3,7 @@
  */
 GRW.init = function() {
   GRW.log('Google Reader Watcher ###VERSION### initializitaion started');
+  GRW.strings = document.getElementById('grwatcher-strings');
   var getlist = new GRW.GetList();
   getlist.on('unreadGeneratedEvent', function(elems) {
     GRW.log('unread generated event');
@@ -12,6 +13,7 @@ GRW.init = function() {
   });
   getlist.on('itemsMatchedEvent', function() {
     GRW.log('items matched event');
+    GRW.Notifier(this._unreadCount.unreadSum);
   })
   getlist.on('unreadAndSubscriptionReceivedEvent', function() {
     this.matchUnreadItems();
