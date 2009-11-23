f62ce73f1637150d7709f12e278e4d46bbad7faa
Lajos Koszti <ajnasz@ajnasz.hu>
2009-11-23 19:13:20 +0100

    cleanup, module registartion:
      remove not used modules
      add GRW.module method which should be used registering new modules,
      namespaces. Everything which is related to the GRW project should be
      registered with that method

--- chrome/content/core.js
+++ chrome/content/core.js
@@ -2,6 +2,16 @@ let GRW = {
   JSON: null
 };
 Components.utils.import("resource://grwmodules/JSON.jsm", GRW);
+
+
+
+GRW.module = function(moduleName, module) {
+  var undef;
+  if(GRW[moduleName] == undef) {
+    GRW[moduleName] = module || {};
+  }
+};
+
 (function(){
 
   var lang = {
@@ -39,9 +49,7 @@ Components.utils.import("resource://grwmodules/JSON.jsm", GRW);
    *
    * @module GRW
    */
-  GRW.lang = lang;
-})();
-(function() {
+
   /**
    * Logger method which writes messages to the error console
    * @method log
@@ -50,17 +58,15 @@ Components.utils.import("resource://grwmodules/JSON.jsm", GRW);
    *
    * @param {String} message log on the javascript console
    */
-  GRW.log = function() {
+  var consoleService = Components.classes["@mozilla.org/consoleservice;1"].getService(Components.interfaces.nsIConsoleService);
+  var log = function() {
     var msg = [];
     for(var i = 0, al = arguments.length; i< al; i++) {
       msg.push(arguments[i]);
     }
-    var consoleService = Components.classes["@mozilla.org/consoleservice;1"].getService(Components.interfaces.nsIConsoleService);
     consoleService.logStringMessage('GRW: ' + msg.join(',\n'));
   };
-})();
 
-(function() {
   var timer = Components.classes["@mozilla.org/timer;1"]
                 .createInstance(Components.interfaces.nsITimer),
       settimeout = function(fn, timeout) {
@@ -73,130 +79,73 @@ Components.utils.import("resource://grwmodules/JSON.jsm", GRW);
           timer.cancel();
         }
       };
-  GRW.setTimeout = settimeout;
-  GRW.clearTimeout = cleartimeout;
-})();
-
-
-/**
- * A SandBox module, to handle the GRW Modules
- * @module SandBox
- *
- * @namespace GRW
- */
-GRW.SandBox = {
-  modules: {},
   /**
-   * Register a module into the grw sandbox
+   * Applies all properties in the supplier to the receiver if the
+   * receiver does not have these properties yet.  Optionally, one or 
+   * more methods/properties can be specified (as additional 
+   * parameters).  This option will overwrite the property if receiver 
+   * has it already.  If true is passed as the third parameter, all 
+   * properties will be applied and _will_ overwrite properties in 
+   * the receiver.
    *
-   * @method register
-   *
-   * @param {String} name The name of the module
-   * @param {Array} [depedencies] Module depedencies
+   * @method augmentObject
+   * @namespace GRW
+   * @static
+   * @since 2.3.0
+   * @see YAHOO.lang.augmentObject
+   * @param {Function} r  the object to receive the augmentation
+   * @param {Function} s  the object that supplies the properties to augment
+   * @param {String*|boolean}  arguments zero or more properties methods 
+   *        to augment the receiver with.  If none specified, everything
+   *        in the supplier will be used unless it would
+   *        overwrite an existing property in the receiver. If true
+   *        is specified as the third parameter, all properties will
+   *        be applied and will overwrite an existing property in
+   *        the receiver
    */
-  register: function (name, depedencies) {
-  },
-  /**
-   * Notifies the registered modules about events
-   * 
-   * @method notify
-   *
-   * @param {String} notifier The name of the module which did an event
-   * @param {String} event The name of the event which has been fired
-   * @param {Array | Object | String} [context] Arguments, objects, etc. which could be passed to another module
-   */
-  notify: function(notifier, event, context) {
-    var module;
-    for(var moduleName in this.modules) {
-      module = this.modules[moduleName];
-      module.happening(notifier, event, context);
+  augmentObject = function(r, s) {
+    if (!s||!r) {
+        throw new Error("Absorb failed, verify dependencies.");
     }
-  }
-};
-
-
-/**
- *
- */
-GRW.Module = function() {
-};
-GRW.Module.prototype = {
-  register: function() {
-    GRW.SandBox.register(this.name, this.depedencies);
-  },
-  happening: function(notifier, event, context) {
-  }
-};
-
-/**
- * Applies all properties in the supplier to the receiver if the
- * receiver does not have these properties yet.  Optionally, one or 
- * more methods/properties can be specified (as additional 
- * parameters).  This option will overwrite the property if receiver 
- * has it already.  If true is passed as the third parameter, all 
- * properties will be applied and _will_ overwrite properties in 
- * the receiver.
- *
- * @method augmentObject
- * @static
- * @since 2.3.0
- * @see YAHOO.lang.augmentObject
- * @param {Function} r  the object to receive the augmentation
- * @param {Function} s  the object that supplies the properties to augment
- * @param {String*|boolean}  arguments zero or more properties methods 
- *        to augment the receiver with.  If none specified, everything
- *        in the supplier will be used unless it would
- *        overwrite an existing property in the receiver. If true
- *        is specified as the third parameter, all properties will
- *        be applied and will overwrite an existing property in
- *        the receiver
- */
-GRW.augmentObject = function(r, s) {
-  if (!s||!r) {
-      throw new Error("Absorb failed, verify dependencies.");
-  }
-  var a=arguments, i, p, overrideList=a[2];
-  if (overrideList && overrideList!==true) { // only absorb the specified properties
-    for (i=2; i<a.length; i=i+1) {
-        r[a[i]] = s[a[i]];
-    }
-  } else { // take everything, overwriting only if the third parameter is true
-    for (p in s) {
-      if (overrideList || !(p in r)) {
-        r[p] = s[p];
+    var a=arguments, i, p, overrideList=a[2];
+    if (overrideList && overrideList!==true) { // only absorb the specified properties
+      for (i=2; i<a.length; i=i+1) {
+          r[a[i]] = s[a[i]];
+      }
+    } else { // take everything, overwriting only if the third parameter is true
+      for (p in s) {
+        if (overrideList || !(p in r)) {
+          r[p] = s[p];
+        }
       }
     }
-  }
-};
+  };
 
-/**
- * Same as GRW.augmentObject, except it only applies prototype properties
- * @see GRW.augmentObject
- * @see YAHOO.lang.augmentProto
- * @method augmentProto
- * @static
- * @param {Function} r  the object to receive the augmentation
- * @param {Function} s  the object that supplies the properties to augment
- * @param {String*|boolean}  arguments zero or more properties methods
- *        to augment the receiver with.  If none specified, everything
- *        in the supplier will be used unless it would overwrite an existing
- *        property in the receiver.  if true is specified as the third
- *        parameter, all properties will be applied and will overwrite an
- *        existing property in the receiver
- */
-GRW.augmentProto = function(r, s) {
-  if (!s||!r) {
-      throw new Error("Augment failed, verify dependencies.");
-  }
-  var a=[r.prototype,s.prototype], i;
-  for (i=2;i<arguments.length;i=i+1) {
-      a.push(arguments[i]);
-  }
-  GRW.augmentObject.apply(this, a);
-};
-
-
-(function(){
+  /**
+   * Same as GRW.augmentObject, except it only applies prototype properties
+   * @see GRW.augmentObject
+   * @see YAHOO.lang.augmentProto
+   * @method augmentProto
+   * @static
+   * @param {Function} r  the object to receive the augmentation
+   * @param {Function} s  the object that supplies the properties to augment
+   * @param {String*|boolean}  arguments zero or more properties methods
+   *        to augment the receiver with.  If none specified, everything
+   *        in the supplier will be used unless it would overwrite an existing
+   *        property in the receiver.  if true is specified as the third
+   *        parameter, all properties will be applied and will overwrite an
+   *        existing property in the receiver
+   */
+  augmentProto = function(r, s) {
+    if (!s||!r) {
+        throw new Error("Augment failed, verify dependencies.");
+    }
+    var a=[r.prototype,s.prototype], i;
+    for (i=2;i<arguments.length;i=i+1) {
+        a.push(arguments[i]);
+    }
+    GRW.augmentObject.apply(this, a);
+  };
   /**
    * @class EventProvider
    * @namespace GRW
@@ -225,6 +174,13 @@ GRW.augmentProto = function(r, s) {
       }
     }
   };
-  GRW.EventProvider = eventProvider;
-})();
 
+  GRW.module('lang', lang);
+  GRW.module('log', log);
+  GRW.module('setTimeout', settimeout);
+  GRW.module('clearTimeout', cleartimeout);
+  GRW.module('augmentObject', augmentObject);
+  GRW.module('augmentProto', augmentProto);
+  GRW.module('EventProvider', eventProvider);
+
+})();
