da49777c745ad092fcc38b9f2d46c791ed149be3
Lajos Koszti <ajnasz@ajnasz.hu>
2009-11-25 19:12:14 +0100

    notifier can fire custom events

--- chrome/content/notifier.js
+++ chrome/content/notifier.js
@@ -1,54 +1,64 @@
 (function() {
-  var showNotification = true;
-  var clickCallback = {
-    observe: function(subject, topic, data) {
-      if(topic == 'alertclickcallback') {
-        showNotification = true;
-      }
-    }
+  var showNotification = true,
+      getPrefs = GRW.Prefs.get;
+
+  var notifier = function() {
   };
+  notifier.prototype = {
+    notificationWin: function(value) {
+      if(getPrefs.showNotificationWindow() !== false) {
+        var label = 'Google Reader Watcher',
+            image = "chrome://grwatcher/skin/grwatcher.png",
+            alertsService,
+            alertWin,
+            _this = this;
 
-  var notificationWin = function(value) {
-    if(GRW.Prefs.get.showNotificationWindow() !== false) {
-      var label = 'Google Reader Watcher',
-          image = "chrome://grwatcher/skin/grwatcher.png",
-          alertsService,
-          alertWin;
+        var clickCallback = {
+          observe: function(subject, topic, data) {
+            if(topic == 'alertclickcallback') {
+              _this.fireEvent('notifierClicked');
+              showNotification = true;
+            }
+          }
+        };
 
-      if(!value) {
-        value = 'Google Reader Watcher Notification';
-      }
-      try {
-        // notifier for windows
-        alertsService = Components.classes["@mozilla.org/alerts-service;1"]
-                          .etService(Components.interfaces.nsIAlertsService);
 
-        alertsService.showAlertNotification(image , label, value, true, "", clickCallback);
-      } catch(e) {
+        if(!value) {
+          value = 'Google Reader Watcher Notification';
+        }
         try {
-        // notifier for linux
-          alertWin = Components.classes["@mozilla.org/embedcomp/window-watcher;1"]
-                      .getService(Components.interfaces.nsIWindowWatcher)
-                      .openWindow(null, "chrome://global/content/alerts/alert.xul", "_blank", "chrome,titlebar=no,popup=yes", null);
+          // notifier for windows
+          alertsService = Components.classes["@mozilla.org/alerts-service;1"]
+                            .getService(Components.interfaces.nsIAlertsService);
 
-            alertWin.arguments = [image, label, value, true, "", 0, clickCallback];
-            setTimeout(function(){alertWin.close()}, 10000);
+          alertsService.showAlertNotification(image , label, value, true, "", clickCallback);
         } catch(e) {
-          GRW.log(e.message);
+          try {
+          // notifier for linux
+            alertWin = Components.classes["@mozilla.org/embedcomp/window-watcher;1"]
+                        .getService(Components.interfaces.nsIWindowWatcher)
+                        .openWindow(null, "chrome://global/content/alerts/alert.xul", "_blank", "chrome,titlebar=no,popup=yes", null);
+
+              alertWin.arguments = [image, label, value, true, "", 0, clickCallback];
+              GRW.setTimeout(function(){alertWin.close()}, 10000);
+          } catch(e) {
+            GRW.log(e.message);
+          }
         }
       }
-    }
-  };
-  var notifier = function(unreadCount) {
-    if(showNotification && unreadCount > 0) {
+    },
+    show: function(unreadCount) {
+      if(showNotification && unreadCount > 0) {
         if(GRW.strings) {
           // it doesn't work in fennec yet
-          notificationWin(GRW.strings.getFormattedString('notifierMSG', [unreadCount]));
+          this.notificationWin(GRW.strings.getFormattedString('notifierMSG', [unreadCount]));
         } else {
-          notificationWin('You have ' + unreadCount + ' unread feeds');
+          this.notificationWin('You have ' + unreadCount + ' unread feeds');
         }
-      showNotification = false;
+        showNotification = false;
+      }
     }
   };
+  GRW.augmentProto(notifier, GRW.EventProvider);
   GRW.module('Notifier', notifier);
 })();
