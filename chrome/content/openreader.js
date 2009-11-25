234f06125434aef06b8d695536b512bc9b1e31e7
Lajos Koszti <ajnasz@ajnasz.hu>
2009-11-25 19:11:09 +0100

    fix opening a reader window
    ok, i can not create alias for everything..

--- chrome/content/openreader.js
+++ chrome/content/openreader.js
@@ -6,10 +6,9 @@
     var outObj = {grTab: false, blankPage: false},
         r = new RegExp('^'+readerURL),
         i = gBrowser.browsers.length - 1,
-        getBrowserAtIndex = gBrowser.getBrowserAtIndex,
         curSpec;
     while(i >= 0) {
-      curSpec = getBrowserAtIndex(i).currentURI.spec;
+      curSpec = gBrowser.getBrowserAtIndex(i).currentURI.spec;
       if(r.test(curSpec)) {
         outObj.grTab = i;
         break;
@@ -24,51 +23,59 @@
     return outObj;
   };
 
-  var openReader = function(subUrl) {
-    var url = subUrl ? readerURL + '/' + subUrl : readerURL;
-        openedGR = getOpenedGR(),
-        selectedIndex = gBrowser.mTabContainer.selectedIndex,
-        currentContent = getBrowserAtIndex(selectedIndex).contentWindow,
-        addTab = gBrowser.addTab;
-    /**
-     * google reader doesn't opened yet
-     */
-    if(openedGR.grTab === false) {
-      /**
-       * open in new tab
-       */
-      if(getPref.openInNewTab()) {
+  var openReader = function() {
+  };
+  openReader.prototype = {
+    open: function(subUrl) {
+      try {
+        var url = subUrl ? readerURL + '/' + subUrl : readerURL;
+            openedGR = getOpenedGR(),
+            currentContent = gBrowser.getBrowserAtIndex(gBrowser.mTabContainer.selectedIndex).contentWindo;
         /**
-         * isn't there any blank page
-         */
-        if(openedGR.blankPage === false) {
-          if(getPref.activateOpenedTab()) {
-            gBrowser.selectedTab = addTab(subUrl);
-            currentContent.focus();
+        * google reader doesn't opened yet
+        */
+        if(openedGR.grTab === false) {
+          /**
+          * open in new tab
+          */
+          if(getPref.openInNewTab()) {
+            /**
+            * isn't there any blank page
+            */
+            if(openedGR.blankPage === false) {
+              if(getPref.activateOpenedTab()) {
+                gBrowser.selectedTab = gBrowser.addTab(url);
+                currentContent.focus();
+              } else {
+                gBrowser.addTab(url);
+              }
+            } else {
+              /**
+              * load the GR into the blank page
+              */
+              if(getPref.activateOpenedTab()) {
+                gBrowser.mTabContainer.selectedIndex = openedGR.blankPage;
+                gBrowser.loadURI(url);
+                currentContent.focus();
+              } else {
+                gBrowser.getBrowserAtIndex(openedGR.blankPage).loadURI(url);
+              }
+            }
           } else {
-            addTab(subUrl);
+            gBrowser.loadURI(url);
           }
         } else {
-          /**
-           * load the GR into the blank page
-           */
-          if(getPref.activateOpenedTab()) {
-            selectedIndex = openedGR.blankPage;
-            gBrowser.loadURI(url);
-            currentContent.focus();
-          } else {
-            getBrowserAtIndex(openedGR.blankPage).loadURI(url);
-          }
+          gBrowser.mTabContainer.selectedIndex = openedGR.grTab;
+          gBrowser.loadURI(url);
         }
-      } else {
-        gBrowser.loadURI(url);
+      } catch(e) {
+        GRW.log('reader open', e);
+        GRW.log('fileName', e.fileName);
+        GRW.log('line', e.lineNumber);
       }
-    } else {
-      selectedIndex = openedGR.grTab;
-      gBrowser.loadURI(url);
+      this.fireEvent('readerOpened');
     }
-    this.fireEvent('readerOpened');
   };
   GRW.augmentProto(openReader, GRW.EventProvider);
-  GRW.module('OpenReader', openReader);
+  GRW.module('OpenReader', new openReader);
 })();
