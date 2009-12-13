c47ab8148a58c61af7296c286048d8f83b8ad6f8
Lajos Koszti <ajnasz@ajnasz.hu>
2009-12-13 15:18:27 +0100

    reorganize grprefs.js, fix get/set password

--- chrome/content/grprefs.js
+++ chrome/content/grprefs.js
@@ -1,86 +1,85 @@
-/**
- * @author Koszti Lajos [Ajnasz] http://ajnasz.hu ajnasz@ajnasz.hu
- * @licence GPL v2
- */
-/**
- * mozilla preferences component service
- * @property prefManager
- * @namespace GRW
- */
-GRW.prefManager = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
-/**
- * save the preferences into the chrome when the pref dialog is accepted
- * @method savePreferences
- * @namespace GRW
- */
-GRW.savePreferences = function() {
-  GRW.Prefs.setPref.checkFreq(document.getElementById('GRW-checkfreq-field').value);
-  GRW.Prefs.setPref.delayStart(document.getElementById('GRW-delayStart-field').value);
-  GRW.Prefs.setPref.openInNewTab(document.getElementById('GRW-openinnewtab-field').checked);
-  GRW.Prefs.setPref.resetCounter(document.getElementById('GRW-resetcounter-field').checked);
-  GRW.Prefs.setPref.tooltipCounterPos(document.getElementById('GRW-tooltipcounterpos-field').value);
-  GRW.Prefs.setPref.tooltipTitleLength(document.getElementById('GRW-tooltiptitlelength-field').value);
-  GRW.Prefs.setPref.rememberLogin(document.getElementById('GRW-rememberLogin-field').checked);
-  GRW.Prefs.setPref.leftClickOpen(document.getElementById('GRW-leftclickopen-field').value);
-  GRW.Prefs.setPref.activateOpenedTab(document.getElementById('GRW-activateopenedtab-field').checked);
-  GRW.Prefs.setPref.showNotificationWindow(document.getElementById('GRW-shownotificationwindow-field').checked);
-  GRW.Prefs.setPref.showZeroCounter(document.getElementById('GRW-showzerocounter-field').checked);
-  GRW.Prefs.setPref.useSecureConnection(document.getElementById('GRW-usesecureconnection-field').checked);
-  GRW.Prefs.setPref.sortByLabels(document.getElementById('GRW-sortbylabels-field').checked);
-  GRW.Prefs.setPref.filteredLabels(document.getElementById('GRW-filteredlabels-field').value);
-  GRW.Prefs.setPref.maximizeCounter(document.getElementById('GRW-maximizecounter-field').checked);
-  GRW.Prefs.setPref.showitemsintooltip(document.getElementById('GRW-showitemsintooltip-field').checked);
-  GRW.Prefs.setPref.showitemsincontextmenu(document.getElementById('GRW-showitemsincontextmenu-field').checked);
+(function() {
+  /**
+   * save the preferences into the chrome when the pref dialog is accepted
+   * @method savePreferences
+   * @namespace GRW
+   */
+  var doc = document,
+      setPref = GRW.Prefs.set,
+      getPref = GRW.Prefs.get;
 
-  GRW.Prefs.setPref.userName(document.getElementById('GRW-accountmanage-email').value);
-  GRW.Prefs.setPref.forceLogin(document.getElementById('GRW-forceLogin-field').checked);
-  GRW.PasswordManager.addPassword(document.getElementById('GRW-accountmanage-pass').value);
-
-};
-/**
- * sets the values on the pref dialog when it opens
- * @method setPrefPaneVals
- * @namespace GRW
- */
-GRW.setPrefPaneVals = function() {
-  document.getElementById('GRW-checkfreq-field').value = GRW.Prefs.getPref.checkFreq();
-  document.getElementById('GRW-delayStart-field').value = GRW.Prefs.getPref.delayStart();
-  document.getElementById('GRW-openinnewtab-field').checked = GRW.Prefs.getPref.openInNewTab();
-  document.getElementById('GRW-resetcounter-field').checked = GRW.Prefs.getPref.resetCounter();
-  document.getElementById('GRW-tooltipcounterpos-field').value = GRW.Prefs.getPref.tooltipCounterPos();
-  document.getElementById('GRW-tooltiptitlelength-field').value = GRW.Prefs.getPref.tooltipTitleLength();
-  document.getElementById('GRW-rememberLogin-field').checked = GRW.Prefs.getPref.rememberLogin();
-  document.getElementById('GRW-leftclickopen-field').value = GRW.Prefs.getPref.leftClickOpen();
-  document.getElementById('GRW-activateopenedtab-field').checked = GRW.Prefs.getPref.activateOpenedTab();
-  document.getElementById('GRW-accountmanage-pass').value = GRW.PasswordManager.getPassword() || '';
-  document.getElementById('GRW-accountmanage-email').value = GRW.Prefs.getPref.userName();
-  document.getElementById('GRW-shownotificationwindow-field').checked = GRW.Prefs.getPref.showNotificationWindow();
-  document.getElementById('GRW-showzerocounter-field').checked = GRW.Prefs.getPref.showZeroCounter();
-  document.getElementById('GRW-usesecureconnection-field').checked = GRW.Prefs.getPref.useSecureConnection();
-  document.getElementById('GRW-sortbylabels-field').checked = GRW.Prefs.getPref.sortByLabels();
-  document.getElementById('GRW-filteredlabels-field').value = GRW.Prefs.getPref.filteredLabels();
-  document.getElementById('GRW-maximizecounter-field').checked = GRW.Prefs.getPref.maximizeCounter();
-  document.getElementById('GRW-forceLogin-field').checked = GRW.Prefs.getPref.forceLogin();
-  document.getElementById('GRW-showitemsintooltip-field').checked = GRW.Prefs.getPref.showitemsintooltip();
-  document.getElementById('GRW-showitemsincontextmenu-field').checked = GRW.Prefs.getPref.showitemsincontextmenu();
-};
-/**
- * show/hide the newtab options
- * @method openNewTabCheckToogle
- * @namespace GRW
- */
-GRW.openNewTabCheckToogle = function() {
-  var cbfield = document.getElementById('GRW-openinnewtab-field');
-  if(cbfield.checked) {
-    document.getElementById('GRW-activateopenedtab-field').disabled = '';
-    document.getElementById('GRW-leftclickopen-field').disabled = '';
-    document.getElementById('GRW-leftclickopen-label').disabled = '';
-
-    // document.getElementById('GRW-openinnewtab-options').style.display = '';
-  } else {
-    document.getElementById('GRW-activateopenedtab-field').disabled = 'disabled';
-    document.getElementById('GRW-leftclickopen-field').disabled = 'disabled';
-    document.getElementById('GRW-leftclickopen-label').disabled = 'disabled';
-    // document.getElementById('GRW-openinnewtab-options').style.display = 'none';
-  }
-};
+  var savePreferences = function() {
+    setPref.checkFreq(doc.getElementById('GRW-checkfreq-field').value);
+    setPref.delayStart(doc.getElementById('GRW-delayStart-field').value);
+    setPref.openInNewTab(doc.getElementById('GRW-openinnewtab-field').checked);
+    setPref.resetCounter(doc.getElementById('GRW-resetcounter-field').checked);
+    setPref.tooltipCounterPos(doc.getElementById('GRW-tooltipcounterpos-field').value);
+    setPref.tooltipTitleLength(doc.getElementById('GRW-tooltiptitlelength-field').value);
+    setPref.rememberLogin(doc.getElementById('GRW-rememberLogin-field').checked);
+    setPref.leftClickOpen(doc.getElementById('GRW-leftclickopen-field').value);
+    setPref.activateOpenedTab(doc.getElementById('GRW-activateopenedtab-field').checked);
+    setPref.showNotificationWindow(doc.getElementById('GRW-shownotificationwindow-field').checked);
+    setPref.showZeroCounter(doc.getElementById('GRW-showzerocounter-field').checked);
+    setPref.useSecureConnection(doc.getElementById('GRW-usesecureconnection-field').checked);
+    setPref.sortByLabels(doc.getElementById('GRW-sortbylabels-field').checked);
+    setPref.filteredLabels(doc.getElementById('GRW-filteredlabels-field').value);
+    setPref.maximizeCounter(doc.getElementById('GRW-maximizecounter-field').checked);
+    setPref.showitemsintooltip(doc.getElementById('GRW-showitemsintooltip-field').checked);
+    setPref.showitemsincontextmenu(doc.getElementById('GRW-showitemsincontextmenu-field').checked);
+  
+    setPref.userName(doc.getElementById('GRW-accountmanage-email').value);
+    setPref.forceLogin(doc.getElementById('GRW-forceLogin-field').checked);
+    GRW.PasswordManager.addPassword(doc.getElementById('GRW-accountmanage-pass').value);
+  
+  };
+  /**
+   * sets the values on the pref dialog when it opens
+   * @method setPrefPaneVals
+   * @namespace GRW
+   */
+  var setPrefPaneVals = function() {
+    doc.getElementById('GRW-checkfreq-field').value = getPref.checkFreq();
+    doc.getElementById('GRW-delayStart-field').value = getPref.delayStart();
+    doc.getElementById('GRW-openinnewtab-field').checked = getPref.openInNewTab();
+    doc.getElementById('GRW-resetcounter-field').checked = getPref.resetCounter();
+    doc.getElementById('GRW-tooltipcounterpos-field').value = getPref.tooltipCounterPos();
+    doc.getElementById('GRW-tooltiptitlelength-field').value = getPref.tooltipTitleLength();
+    doc.getElementById('GRW-rememberLogin-field').checked = getPref.rememberLogin();
+    doc.getElementById('GRW-leftclickopen-field').value = getPref.leftClickOpen();
+    doc.getElementById('GRW-activateopenedtab-field').checked = getPref.activateOpenedTab();
+    doc.getElementById('GRW-accountmanage-email').value = getPref.userName();
+    doc.getElementById('GRW-shownotificationwindow-field').checked = getPref.showNotificationWindow();
+    doc.getElementById('GRW-showzerocounter-field').checked = getPref.showZeroCounter();
+    doc.getElementById('GRW-usesecureconnection-field').checked = getPref.useSecureConnection();
+    doc.getElementById('GRW-sortbylabels-field').checked = getPref.sortByLabels();
+    doc.getElementById('GRW-filteredlabels-field').value = getPref.filteredLabels();
+    doc.getElementById('GRW-maximizecounter-field').checked = getPref.maximizeCounter();
+    doc.getElementById('GRW-forceLogin-field').checked = getPref.forceLogin();
+    doc.getElementById('GRW-showitemsintooltip-field').checked = getPref.showitemsintooltip();
+    doc.getElementById('GRW-showitemsincontextmenu-field').checked = getPref.showitemsincontextmenu();
+    doc.getElementById('GRW-accountmanage-pass').value = GRW.PasswordManager.getPassword() || '';
+  };
+  /**
+   * show/hide the newtab options
+   * @method openNewTabCheckToogle
+   * @namespace GRW
+   */
+  var openNewTabCheckToogle = function() {
+    var cbfield = doc.getElementById('GRW-openinnewtab-field');
+    if(cbfield.checked) {
+      doc.getElementById('GRW-activateopenedtab-field').disabled = '';
+      doc.getElementById('GRW-leftclickopen-field').disabled = '';
+      doc.getElementById('GRW-leftclickopen-label').disabled = '';
+  
+      // document.getElementById('GRW-openinnewtab-options').style.display = '';
+    } else {
+      doc.getElementById('GRW-activateopenedtab-field').disabled = 'disabled';
+      doc.getElementById('GRW-leftclickopen-field').disabled = 'disabled';
+      doc.getElementById('GRW-leftclickopen-label').disabled = 'disabled';
+      // document.getElementById('GRW-openinnewtab-options').style.display = 'none';
+    }
+  };
+  GRW.module('SavePreferences', savePreferences);
+  GRW.module('SetPrefPaneVals', setPrefPaneVals);
+  GRW.module('OpenNewTabCheckToogle', openNewTabCheckToogle);
+})();
