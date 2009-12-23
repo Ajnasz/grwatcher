d21dbcb4094aa132a04c4ec90865ee2ca87c6396
Lajos Koszti <ajnasz@ajnasz.hu>
2009-12-23 17:56:02 +0100

    make possible to pass arguments to the callback method when using the mapWindows method

--- chrome/content/ui/mapwindows.js
+++ chrome/content/ui/mapwindows.js
@@ -2,13 +2,21 @@
   var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"]
             .getService(Components.interfaces.nsIWindowMediator);
 
-  var mapwindows = function(fn, scope) {
+var mapwindows = function(fn, args, scope) {
     var win,
         enumerator = wm.getEnumerator('navigator:browser');
     while(enumerator.hasMoreElements()) {
       win = enumerator.getNext();
       if(typeof win != 'undefined') {
-        fn(win);
+        if(!scope) {
+          scope = win;
+        }
+        if(!GRW.lang.isArray(args)) {
+          args = [win];
+        } else {
+          args.unshift(win);
+        }
+        fn.apply(scope, args);
       }
     }
   };
