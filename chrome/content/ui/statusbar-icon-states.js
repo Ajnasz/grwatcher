0e2afbdc2264bba97863a1505f6649195a09dc3e
Lajos Koszti <ajnasz@ajnasz.hu>
2010-03-25 23:00:27 +0100

    use new icons on the statusbar

--- chrome/content/ui/statusbar-icon-states.js
+++ chrome/content/ui/statusbar-icon-states.js
@@ -11,24 +11,22 @@
         if(!statusImage) {return;}
         switch(status) {
           case 'on':
-            statusImage.src = 'chrome://grwatcher/content/images/googlereader.png';
+            statusImage.src = 'chrome://grwatcher/skin/feed-icon-statusbar.png';
+            break;
+
+
+          case 'error':
+          case 'cookieerror':
+            statusImage.src = 'chrome://grwatcher/skin/feed-icon-error-statusbar.png';
+            break;
+
+          case 'load':
+            statusImage.src = 'chrome://grwatcher/skin/loading-small.gif';
             break;
 
           case 'off':
           default:
-            statusImage.src = 'chrome://grwatcher/content/images/googlereader_grey.png';
-            break;
-
-          case 'error':
-            statusImage.src = 'chrome://grwatcher/content/images/googlereader_red.png';
-            break;
-
-          case 'cookieerror':
-            statusImage.src = 'chrome://grwatcher/content/images/googlereader_red.png';
-            break;
-
-          case 'load':
-            statusImage.src = 'chrome://grwatcher/content/images/loader.gif';
+            statusImage.src = 'chrome://grwatcher/skin/feed-icon-inactive-statusbar.png';
             break;
         }
       });
