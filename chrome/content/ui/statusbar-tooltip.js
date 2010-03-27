10c26c6c3bb60660bf0301ec2c9ea936a5798dae
Lajos Koszti <ajnasz@ajnasz.hu>
2010-03-27 11:43:40 +0100

    set the tooltip grid also for the toolbar button

--- chrome/content/ui/statusbar-tooltip.js
+++ chrome/content/ui/statusbar-tooltip.js
@@ -3,16 +3,28 @@
   const statusbarID = 'GRW-statusbar',
     toolbarButtonID = 'GRW-toolbar-button';
 
+  var setTooltip = function(win, statusbarTooltip, toolbarTooltip) {
+      var statusbar = win.document.getElementById(statusbarID);
+      var ttb = win.document.getElementById(toolbarButtonID);
+      if(statusbar) {
+        statusbar.tooltip = statusbarTooltip;
+      }
+      if(ttb) {
+        ttb.tooltip = toolbarTooltip;
+      }
+  };
+
   var actions = {
     genGrid: function(win, feeds, getlist) {
-      var statusbar = win.document.getElementById(statusbarID);
-      var activeGRW = GRW.getActiveGRW().GRW; 
+      var activeGRW = GRW.getActiveGRW().GRW;
       var getlist = activeGRW.GetList;
       var feeds = activeGRW.feeds;
+      var labels = getlist.getLabels();
+      // statusbar
+      var statusbar = win.document.getElementById(statusbarID);
       if(statusbar) {
         statusbar.tooltip = 'GRW-statusbar-tooltip-new';
         var tooltipContainer = win.document.getElementById('GRW-statusbar-tooltip-new');
-        var labels = getlist.getLabels();
         var grid = new GRW.UI.Grid(win.document, feeds, labels).getGrid();
         while(tooltipContainer.firstChild) {
           tooltipContainer.removeChild(tooltipContainer.firstChild);
@@ -20,46 +32,35 @@
         tooltipContainer.appendChild(grid);
         new Menu(win, feeds, labels);
       }
+      // toolbar
+      var toolbar = win.document.getElementById(toolbarButtonID);
+      if(toolbar) {
+        toolbar.tooltip = 'GRW-statusbar-tooltip-new';
+        var tooltipContainer = win.document.getElementById('GRW-toolbar-tooltip-new');
+        var grid = new GRW.UI.Grid(win.document, feeds, labels).getGrid();
+        if(tooltipContainer) {
+          while(tooltipContainer.firstChild) {
+            tooltipContainer.removeChild(tooltipContainer.firstChild);
+          }
+          tooltipContainer.appendChild(grid);
+        }
+        // new Menu(win, feeds, labels);
+      }
     },
     error: function(win) {
-      var statusbar = win.document.getElementById(statusbarID);
-      var ttb = win.document.getElementById(toolbarButtonID);
-      if(statusbar) {
-        statusbar.tooltip = 'GRW-statusbar-tooltip-error';
-        if(ttb)ttb.setAttribute('tooltiptext', GRW.strings.getString('errorfeedfetch'));
-      }
+      setTooltip(win, 'GRW-statusbar-tooltip-error', 'GRW-toolbar-tooltip-error');
     },
     nonew: function(win) {
-      var statusbar = win.document.getElementById(statusbarID);
-      var ttb = win.document.getElementById(toolbarButtonID);
-      if(statusbar) {
-        statusbar.tooltip = 'GRW-statusbar-tooltip-nonew';
-        if(ttb) ttb.setAttribute('tooltiptext', GRW.strings.getString('nonewfeed'));
-      }
+      setTooltip(win, 'GRW-statusbar-tooltip-nonew','GRW-toolbar-tooltip-nonew');
     },
     cookieError: function(win) {
-      var statusbar = win.document.getElementById(statusbarID);
-      var ttb = win.document.getElementById(toolbarButtonID);
-      if(statusbar) {
-        statusbar.tooltip = 'GRW-statusbar-tooltip-cookieerror';
-        if(ttb) ttb.setAttribute('tooltiptext', GRW.strings.getString('cookieerror'));
-      }
+      setTooltip(win, 'GRW-statusbar-tooltip-cookieerror','GRW-toolbar-tooltip-cookieerror');
     },
     networkError: function(win) {
-      var statusbar = win.document.getElementById(statusbarID);
-      var ttb = win.document.getElementById(toolbarButtonID);
-      if(statusbar) {
-        statusbar.tooltip = 'GRW-statusbar-tooltip-networkerror';
-        if(ttb) ttb.setAttribute('tooltiptext', GRW.strings.getString('networkerror'));
-      }
+      setTooltip(win, 'GRW-statusbar-tooltip-networkerror', 'GRW-ttb-tooltip-networkerror');
     },
     loginError: function(win) {
-      var statusbar = win.document.getElementById(statusbarID);
-      var ttb = win.document.getElementById(toolbarButtonID);
-      if(statusbar) {
-        statusbar.tooltip = 'GRW-statusbar-tooltip-loginerror';
-        if(ttb) ttb.setAttribute('tooltiptext', GRW.strings.getString('errorlogin'));
-      }
+      setTooltip(win, 'GRW-statusbar-tooltip-loginerror', 'GRW-statusbar-tooltip-loginerror');
     },
   };
   var statusbarTooltip = function(action, feeds, getlist) {
