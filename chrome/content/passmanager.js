d8c090c9a1bbf9fb61073e3d7257413da79a2552
Lajos Koszti <ajnasz@ajnasz.hu>
2010-03-28 11:49:57 +0200

    if the user has the login information already in the password manager because he saved it from the google login form, use that data

--- chrome/content/passmanager.js
+++ chrome/content/passmanager.js
@@ -13,8 +13,46 @@
    * @namespace GRW
    */
   var _PasswordManager = function() {
+    var _googlePW = null;
+    /**
+     * Get the already stored google login info
+     * @method _getGooglePW
+     * @private
+     * @returns false if info not found, login data if found
+     * @type nsiLoginInfo
+     */
+    var _getGooglePW = function() {
+      if(_googlePW === null) {
+        /**
+         * list all of the login infos
+         */
+        /*
+        var logins = loginManager.getAllLogins({});
+        logins.forEach(function(l) {
+          GRW.log(l.hostname, l.formSubmitURL, l.httpRealm, l.username, l.password, l.usernameField, l.passwordField);
+        });
+        */
+        var logins = loginManager.findLogins({}, 'https://www.google.com', 'https://www.google.com', null);
+        if(logins.length) {
+          _googlePW = logins[0];
+        } else {
+          _googlePW = false;
+        }
+      }
+      return _googlePW;
+    };
+    /**
+     * method to get the user's password to login to google service
+     * @method getPassword
+     * @returns the password or false if it si not found
+     * @type {String,Boolean}
+     */
     this.getPassword = function() {
       try {
+        var googlePW = _getGooglePW();
+        if(googlePW) {
+          return googlePW.password;
+        }
         // Find users for the given parameters
         var logins = loginManager.findLogins({}, url, formSubmitURL, null);
         // Find user from returned array of nsILoginInfo objects
@@ -24,10 +62,22 @@
           }
         }
         return false;
+      }catch(ex){
+        GRW.log('get pass failed:', ex);
       }
-      catch(ex){
-      GRW.log('get pass failed:', ex);
-      }
+    };
+    /**
+     * Retreives the username
+     * @method getUsername
+     * @returns the username to log in to the google service
+     * @type String
+     */
+    this.getUsername = function() {
+        var googlePW = _getGooglePW();
+        if(googlePW) {
+          return googlePW.username;
+        }
+        return GRW.Prefs.get.userName();
     };
 
     this.addPassword = function(password) {
@@ -48,5 +98,5 @@
     }
   };
   GRW.PasswordManager = new _PasswordManager();
-  GRW.log('passwordmanagaer');
+  // GRW.log('passwordmanagaer');
 })();
