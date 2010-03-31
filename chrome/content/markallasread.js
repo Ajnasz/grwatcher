6eb04a7b54510fee6dc7bb28b973fdece6c469d7
Lajos Koszti <ajnasz@ajnasz.hu>
2010-03-31 21:11:07 +0200

    fix mark all as read functionallity

--- chrome/content/markallasread.js
+++ chrome/content/markallasread.js
@@ -5,52 +5,25 @@
     mark: function() {
       var _this = this;
       if(confirm(GRW.strings.getString('confirmmarkallasread'))) {
-        GRW.Token({
-            success: {
-              fn: this._markAsRead,
-              scope: this,
-            },
-            failure: {
-              fn: function() {
-                this.fireEvent('onMarkAllAsReadFailed');
-              },
-              scope: this,
-            }
-        });
+        _this._markAsRead();
       }
     },
     _markAsRead: function() {
       var _this = this;
-      var parameters = {
-          // T: GRW.token.token,
-          ts: (new Date()).getTime() + '999',
-          s:'user/-/state/com.google/reading-list',
-        };
-        GRW.request('post', GRW.States.conntype + ':www.google.com/reader/api/0/mark-all-as-read?client=scroll', {
-          onSuccess: function(request) {
-            if(this.req.responseText == 'OK') {
-              _this.fireEvent('onMarkAllAsRead');
-            }
-          },
-          onError: function() {
-              _this.fireEvent('onMarkAllAsReadFailed');
-          },
-
-        }, parameters);
-        /*
-        new GRW.Ajax({
-        method: 'post',
-        url: GRW.States.conntype + ':www.google.com/reader/api/0/mark-all-as-read?client=scroll',
-        onSuccess: function(request) {
-          if(this.req.responseText == 'OK') {
-            _this.fireEvent('onMarkAllAsRead');
-          }
-        },
-        onError: function() {
-            _this.fireEvent('onMarkAllAsReadFailed');
-        },
-      }, parameters).send();
-      */
+      GRW.getToken(function() {
+        GRW.request('post',
+          GRW.States.conntype + '://www.google.com/reader/api/0/mark-all-as-read?client=scroll',
+          {
+            onSuccess: function(request) {
+              if(request.responseText == 'OK') {
+                _this.fireEvent('onMarkAllAsRead');
+              }
+            },
+            onError: function() {
+                _this.fireEvent('onMarkAllAsReadFailed');
+            },
+          }, 's=user/-/state/com.google/reading-list&T=' + GRW.token.token);
+      });
     }
   };
   GRW.augmentProto(markAllAsRead, GRW.EventProvider);
