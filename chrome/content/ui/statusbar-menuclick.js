dd072e6e3f25b3a226c99eed0bdc7fe50a7a68b4
Lajos Koszti <ajnasz@ajnasz.hu>
2010-04-13 21:55:32 +0200

    check for element before add listener to it

--- chrome/content/ui/statusbar-menuclick.js
+++ chrome/content/ui/statusbar-menuclick.js
@@ -8,27 +8,41 @@
   var menuClick = function() {};
   menuClick.prototype = {
     init: function() {
-      var _this = this;
-      document.getElementById(openReader).addEventListener('command', function(event) {
-        _this.fireEvent('openReader')
-      }, true);
-      document.getElementById(markAllAsRead).addEventListener('command', function(event) {
-        _this.fireEvent('markAllAsRead');
-      }, true);
-      document.getElementById(checkUnreadFeeds).addEventListener('command', function(event) {
-        _this.fireEvent('checkUnreadFeeds');
-      }, true);
-      document.getElementById(openPreferences).addEventListener('command', function(event) {
-        _this.fireEvent('openPreferences');
-      }, true);
-      document.getElementById(enableCookies).addEventListener('command', function(event) {
-        _this.fireEvent('enableCookies');
-      }, true);
+      var _this = this,
+          _openReader       = document.getElementById(openReader),
+          _markAllAsRed     = document.getElementById(markAllAsRead),
+          _checkUnreadFeeds = document.getElementById(checkUnreadFeeds),
+          _openPreferences  = document.getElementById(openPreferences),
+          _enableCookies    = document.getElementById(enableCookies);
+      if(_openReader) {
+        _openReader.addEventListener('command', function(event) {
+          _this.fireEvent('openReader')
+        }, true);
+      }
+      if(_markAllAsRed) {
+        _markAllAsRed.addEventListener('command', function(event) {
+          _this.fireEvent('markAllAsRead');
+        }, true);
+      }
+      if(_checkUnreadFeeds) {
+        _checkUnreadFeeds.addEventListener('command', function(event) {
+          _this.fireEvent('checkUnreadFeeds');
+        }, true);
+      }
+      if(_openPreferences) {
+        _openPreferences.addEventListener('command', function(event) {
+          _this.fireEvent('openPreferences');
+        }, true);
+      }
+      if(_enableCookies) {
+        _enableCookies.addEventListener('command', function(event) {
+          _this.fireEvent('enableCookies');
+        }, true);
+      }
     }
   };
 
   GRW.augmentProto(menuClick, GRW.EventProvider);
-  
   GRW.module('MenuClick', menuClick);
 
 })();
