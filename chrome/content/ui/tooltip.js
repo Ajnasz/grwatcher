f83a1f4247c6e388358945d629e4d4276d97ab48
Lajos Koszti <ajnasz@ajnasz.hu>
2010-04-15 22:01:25 +0200

    removing cache

--- chrome/content/ui/tooltip.js
+++ chrome/content/ui/tooltip.js
@@ -1,17 +1,17 @@
 (function() {
   var tooltip = function(action, feeds, getlist) {
     var activeGRW = GRW.getActiveGRW().GRW;
-    var cache = {
-      activeGRW: activeGRW,
-      getlist: activeGRW.GetList,
-      feeds: activeGRW.feeds,
-      labels: getlist.getLabels(),
-    };
+    // var cache = {
+    //   activeGRW: activeGRW,
+    //   getlist: activeGRW.GetList,
+    //   feeds: activeGRW.feeds,
+    //   labels: getlist ? getlist.getLabels() : null,
+    // };
     if(GRW.UI.StatusbarTooltip) {
-      GRW.UI.StatusbarTooltip(action, feeds, getlist, cache);
+      GRW.UI.StatusbarTooltip(action, feeds, getlist);
     }
     if(GRW.UI.ToolbarTooltip) {
-      GRW.UI.ToolbarTooltip(action, feeds, getlist, cache);
+      GRW.UI.ToolbarTooltip(action, feeds, getlist);
     }
   };
   GRW.UI.Tooltip = tooltip;
