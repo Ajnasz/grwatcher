7434eb3ed3f4ede93a61436242350c4d0d6f404a
Lajos Koszti <ajnasz@ajnasz.hu>
2011-01-21 22:50:27 -0800

    version to 1.4b

--- makexpi.sh
+++ makexpi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ########################## Configuration ################################
 if [ -z $1 ];then
-  VER='1.3.6b'`date '+%Y%m%d%H%M%S'`'-'`./getver.pl`;
+  VER='1.4'`date '+%Y%m%d%H%M%S'`'-'`./getver.pl`;
 else
   VER=$1;
 fi
