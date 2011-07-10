baee0d846b6d45ba644153b00f375a060aa032fd
Lajos Koszti <ajnasz@ajnasz.hu>
2011-07-10 15:01:01 +0200

    jslint

--- chrome/content/core.js
+++ chrome/content/core.js
@@ -1,7 +1,10 @@
-let GRW = {};
-GRW.module = function(moduleName, module) {
-  if(typeof moduleName === 'string') {
-    if(typeof GRW[moduleName] === 'undefined') {
+/*jslint indent:2*/
+/*global Components:true*/
+var GRW = {};
+var scope = {};
+GRW.module = function (moduleName, module) {
+  if (typeof moduleName === 'string') {
+    if (typeof GRW[moduleName] === 'undefined') {
       GRW[moduleName] = module || {};
     }
     return GRW[moduleName];
@@ -9,44 +12,44 @@ GRW.module = function(moduleName, module) {
   return false;
 };
 
-(function(){
-  Components.utils.import("resource://grwmodules/CustomEvent.jsm");
-
+(function () {
   var lang = {
-    isString: function(arg) {
+    isString: function (arg) {
       return typeof(arg) === 'string';
     },
-    isNumber: function(arg) {
+    isNumber: function (arg) {
       return typeof(arg) === 'number';
     },
-    isBoolean: function(arg) {
+    isBoolean: function (arg) {
       return typeof(arg) === 'boolean';
     },
-    isFunction: function(arg) {
+    isFunction: function (arg) {
       return typeof(arg) === 'function';
     },
-    isNull: function(arg) {
+    isNull: function (arg) {
       return arg === null;
     },
-    isArray: function(arg) {
+    isArray: function (arg) {
       return typeof arg === 'object' && lang.isNumber(arg.length) && lang.isFunction(arg.slice);
     },
-    isObject: function(arg) {
-      return typeof(arg) === 'object' && !lang.isNull(arg) && !lang.isString(arg) && lang.isNumber(arg) && !lang.isFunction(arg) && !lang.isBoolean(arg);
+    isObject: function (arg) {
+      return typeof(arg) === 'object' && !lang.isNull(arg) &&
+      !lang.isString(arg) && lang.isNumber(arg) && !lang.isFunction(arg) &&
+      !lang.isBoolean(arg);
     },
-    isUrl: function(arg) {
-      return lang.isString(arg) && /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/.test(arg);
+    isUrl: function (arg) {
+      return lang.isString(arg) &&
+      /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/.test(arg);
     },
-    isUndef: function(arg) {
-      var undefined;
-      return arg === undefined;
+    isUndef: function (arg) {
+      return typeof arg === 'undefined';
     },
-    toArray: function(arg) {
+    toArray: function (arg) {
       var _a = [];
       return _a.slice.call(arg);
     },
-    isEmail: function(arg) {
-    },
+    isEmail: function (arg) {
+    }
   };
   /**
    * Core JavaScript namespace for the Google Reader Watcher extension
@@ -56,4 +59,4 @@ GRW.module = function(moduleName, module) {
 
 
   GRW.module('lang', lang);
-})();
+}());
